a58bf3f92ba68c619bc48fb4d5c8a4379c37cb41
Steven Bleau <bleausd@gmail.com>
2022-06-13 15:33:55 -0500

    roll the dice

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -19,28 +19,35 @@ let luckyNumber = 7;
 console.log('the value of luckyNumber is', luckyNumber);
 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
-console.log('My name is ',fullName,', and I think ',luckyNumber, 'is a winner!')
+console.log('My name is ',fullName,', and I think ',luckyNumber, 'is a winner!');
 // Refer back to the videos if you need help with this one.
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = 'Taiwanese Beef Noodl Soup';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2;
 // 11 - Add two pets to your `pets` variable
-
+pets += 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 2;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if (adventurous == true){
+    console.log('Adventures are great!');
+}
+else{
+    console.log('how about we stay home?');
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if ((luckyNumber==2)&&(adventurous==true){
+    console.log('Roll the dice!');
+}
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
